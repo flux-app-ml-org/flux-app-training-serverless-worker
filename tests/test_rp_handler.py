003735f3a6cecf1f9d26bab1662d7d0179b578fe
Your Name <sergei.afonchenko42@gmail.com>
2025-06-04 22:05:17 -0300

    fix tests

--- tests/test_rp_handler.py
+++ tests/test_rp_handler.py
@@ -62,7 +62,7 @@ EXPECTED_CONFIG_VALUES = {
     "gradient_checkpointing": True,
     "noise_scheduler": "flowmatch",
     "optimizer": "adamw8bit",
-    "lr": 4e-4,
+    "lr": 1e-4,
     "disable_sampling": True,
     "ema_use": True,
     "ema_decay": 0.99,
